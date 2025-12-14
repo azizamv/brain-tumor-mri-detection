daaf6ab704bb2dd230cfb9f3737e4fda1a13142f
zee <96324043+azizamv@users.noreply.github.com>
2025-12-14 19:40:42 +0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -284,5 +284,5 @@ if __name__ == '__main__':
     print(f"Running on: http://localhost:5000")
     print("="*60 + "\n")
     
-    # Run app
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # # Run app
+    # app.run(debug=True, host='0.0.0.0', port=5000)
